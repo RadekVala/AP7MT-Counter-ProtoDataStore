4c580de54dc0f46ea3b49ed04e326d45a034be25
Radek Vala <radek.www@gmail.com>
2023-10-19 12:52:59 +0200

    MainActivityViewModel.kt - create new viewmodel properties using LiveData (MutableLiveData) for direct data binding in to View

--- app/src/main/java/cz/utb/fai/counterviewmodel/MainActivityViewModel.kt
+++ app/src/main/java/cz/utb/fai/counterviewmodel/MainActivityViewModel.kt
@@ -1,14 +1,32 @@
 package cz.utb.fai.counterviewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
 
-    var email: String = ""
-    var name: String = ""
-    var counter: Int = 0
+    // MutableLiveData for private mutable counter
+    private val _counter = MutableLiveData<Int>()
 
-    fun increment() {
-        counter += 1
+    // Expose LiveData to observe changes
+    val counter: LiveData<Int> get() = _counter
+
+    // MutableLiveData for easy two-way data binding
+    val nameMutable = MutableLiveData<String>()
+    val emailMutable = MutableLiveData<String>()
+
+    // Initial values
+    init {
+        _counter.value = 0
+        nameMutable.value = ""
+        emailMutable.value = ""
     }
+
+    // incrementing of counter
+    fun increment() {
+        // incrementing of LiveData - get the Int number from .value, if not set, it returns 0
+        _counter.value = ( _counter.value ?: 0) + 1
+    }
+
 }
