60c2030756c03c35afaf4da2103ba5723c38d858
Radek Vala <radek.www@gmail.com>
2023-11-13 14:53:15 +0100

    MainActivity.kt - reference to app is created to obtain counterDataStore - viewModel instance is created
    activity_main.xml - click event handler for btn Save added

--- app/src/main/java/cz/utb/fai/counterviewmodel/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/counterviewmodel/MainActivity.kt
@@ -2,6 +2,7 @@ package cz.utb.fai.counterviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
 import androidx.lifecycle.ViewModelProvider
 import cz.utb.fai.counterviewmodel.databinding.ActivityMainBinding
 
@@ -15,7 +16,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        val app = application as MyApplication
+        val dataStore: DataStore<CounterData> = app.counterDataStore
+
+        viewModel = ViewModelProvider(
+            this,
+            MainActivityViewModelFactory(dataStore)
+        ).get(MainActivityViewModel::class.java)
+
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
